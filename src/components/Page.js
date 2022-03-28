c8c4b0d4515ed35921820d7e8ae2c98bc85fe548
KENNY_NGOIE <mukonkolekngoie@gmail.com>
2022-03-28 11:27:39 +0200

    first commit

---
+++ src/components/Page.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import './[PageStyles.css'
+import Lottie from "react-lottie";
+import animationData from '../badge.json'
+
+const Anime = () => {
+    const defaultOptions = {
+        loop: true,
+        autoplay: true,
+        animationData: animationData,
+        rendererSettings: {
+          preserveAspectRatio: "xMidYMid slice"
+        }
+      };
+
+    
+    return (
+        <div className='hero'>
+            <div className='content'>
+            <div>
+         <Lottie options={defaultOptions} alt="ribbon" width={240} height={240} />
+      </div>
+                <h1>Congratulations!</h1>
+                <p>You finished the study. If you had any problem, please leave feedback.</p>
+                <div>
+                    <Link to='/training' className='btn'>All Set</Link>
+                    <Link to='/contact' className='btn btn-light'>Feedback</Link>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default Anime
---
+++ src/routes/Home.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Anime from '../components/Page'
+
+const Page = () => {
+    return (
+        <div>
+            <Anime />
+        </div>
+    )
+}
+
+export default Page
